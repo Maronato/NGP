21d109fc1e96cd08f004efab137815524c1b0335
Maronato <gustavomaronato@gmail.com>
2016-11-25 16:53:52 -0500

    setup instructions for cython

---
+++ setup.py
@@ -0,0 +1,8 @@
+import numpy
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+    ext_modules = cythonize("helpers.pyx"),
+    include_dirs = [numpy.get_include()] #Include directory not hard-wired
+)
