992ac4ba27f5b6e5f31d4fde6472ce01512d6ebb
Maronato <gustavomaronato@gmail.com>
2016-11-29 16:56:40 -0500

    fixed static files not being collected

--- project/urls.py
+++ project/urls.py
@@ -1,27 +1,18 @@
-"""project URL Configuration
+from django.conf.urls import include, url
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
 from django.contrib import admin
+admin.autodiscover()
 from interface import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name='index'),
     url(r'^stats/', views.stats, name='stats'),
     url(r'^info/', views.info, name='info'),
     url(r'^docs/', views.docs, name='docs'),
-    url(r'^fit-predict/', views.fit_predict, name='fit-predict')
+    url(r'^fit-predict/', views.fit_predict, name='fit-predict'),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    url(r'^humans\.txt$', TemplateView.as_view(template_name='humans.txt', content_type='text/plain')),
+
 ]
