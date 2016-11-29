87f40b26b1ef9537ace671eed648d42556d878a5
Maronato <gustavomaronato@gmail.com>
2016-11-28 19:45:45 -0500

    added urls to interface views

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from interface import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    url(r'^stats/', views.stats, name='stats'),
+    url(r'^info/', views.info, name='info'),
+    url(r'^docs/', views.docs, name='docs'),
 ]
