38793caca27c9b6322671929284cf190c9fc9aa9
Maronato <gustavomaronato@gmail.com>
2016-11-30 01:27:55 -0500

    added example url

--- project/urls.py
+++ project/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     url(r'^stats/', views.stats, name='stats'),
     url(r'^info/', views.info, name='info'),
     url(r'^docs/', views.docs, name='docs'),
+    url(r'^example/', views.example, name='example'),
     url(r'^fit-predict/', views.fit_predict, name='fit-predict'),
     url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     url(r'^humans\.txt$', TemplateView.as_view(template_name='humans.txt', content_type='text/plain')),
