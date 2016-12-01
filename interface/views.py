2f44325c2041fe15ccc8988e7575b060a60b88fd
Maronato <gustavomaronato@gmail.com>
2016-12-01 02:14:32 -0500

    added /math/ view

--- interface/views.py
+++ interface/views.py
@@ -1,36 +1,44 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import JsonResponse
 from learning.nmf import NMF
 import numpy as np
-from data import unicamp
+import json
 
 # Create your views here.
+
 
 def index(request):
     return render(request, 'index.html')
 
+
 def stats(request):
     return render(request, 'stats.html')
+
 
 def info(request):
     return render(request, 'info.html')
 
+
 def docs(request):
     return render(request, 'docs.html')
+
 
 def example(request):
     return render(request, 'example.html')
 
+
+def math(request):
+    return render(request, 'math.html')
+
+
 def fit_predict(request):
-    import json
-    from operator import itemgetter
     raw = json.loads(request.POST['data'])
     R = int(request.POST['R'])
     eC = float(request.POST['eC'])
     matrix = []
     for row in raw:
-        l = [float(x[1]) for x in sorted(row.items(), key=lambda s: s[0]) if x[1].isnumeric()]
-        matrix.append(l)
+        cleaned_row = [float(x[1]) for x in sorted(row.items(), key=lambda s: s[0]) if x[1].isnumeric()]
+        matrix.append(cleaned_row)
     matrix = np.array(matrix)
     model = NMF()
     model.fit(matrix, R, eC=eC)
